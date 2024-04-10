83a40dcd7551d4e5550a96a3bab7c283d9d3010f
Artem Gerasimov <artemtrim@mail.ru>
2024-04-10 17:25:34 +0300

    Initial Commit

---
+++ iOS-Avito-test-2024/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-Avito-test-2024
+//
+//  Created by Мой Macbook on 10.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
